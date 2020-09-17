e6a359cae099b56ac972842386a2942e4374aae2
John Verdone <jmverdone@gmail.com>
2020-09-17 18:50:42 -0400

    styling

--- App.js
+++ App.js
@@ -9,6 +9,7 @@
 import React, {useState} from 'react';
 import {
   SafeAreaView,
+  StyleSheet,
   View,
   Text,
   TextInput,
@@ -104,34 +105,43 @@ const App: () => React$Node = () => {
 
   return (
     <SafeAreaView>
-      <View>
-        <TextInput
-          placeholder="Enter Name:"
-          onChangeText={(text) => handleNameChange(text)}
-          value={name}
-        />
-        <TextInput
-          placeholder="Enter Age"
-          onChangeText={(text) => handleAgeChange(text)}
-          value={age}
-        />
-        <Button title="Submit Name and Age" onPress={addPerson} />
+      <View style={styles.container}>
+        <View style={styles.inputWrapper}>
+          <TextInput
+            style={styles.inputbox}
+            placeholder="Enter Name:"
+            onChangeText={(text) => handleNameChange(text)}
+            value={name}
+          />
+          <TextInput
+            style={styles.inputbox}
+            placeholder="Enter Age"
+            onChangeText={(text) => handleAgeChange(text)}
+            value={age}
+          />
+        </View>
+        <TouchableHighlight onPress={addPerson}>
+          <Text style={styles.submitButtonText}>Submit Name and Age</Text>
+        </TouchableHighlight>
       </View>
       <View>
         <FlatList
+          style={styles.list}
           data={personList}
           renderItem={(person, index) => (
-            <View key={index}>
-              <Text>Name: {person.item.name}</Text>
-              <Text>Age: {person.item.age}</Text>
-              <Button
-                title="Remove Person"
-                onPress={() => deletePerson(person)}
-              />
-              <Button
-                title="Edit Person"
-                onPress={() => updatePerson(person)}
-              />
+            <View key={index} style={styles.listItem}>
+              <View style={styles.nameText}>
+                <Text>Name: {person.item.name}</Text>
+                <Text>Age: {person.item.age}</Text>
+              </View>
+              <View style={styles.editButtons}>
+                <TouchableHighlight onPress={() => deletePerson(person)}>
+                  <Text>Remove Person</Text>
+                </TouchableHighlight>                    
+                <TouchableHighlight onPress={() => updatePerson(person)}>
+                  <Text> Edit Person</Text>
+                </TouchableHighlight>
+              </View>
             </View>
           )}
         />
@@ -140,5 +150,41 @@ const App: () => React$Node = () => {
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 25,
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+      
+  },
+  inputWrapper: {
+    marginBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: 250
+  },
+  inputbox: {
+    
+    
+  },
+  submitButtonText: {
+    color: 'white',
+    backgroundColor: 'green',
+    padding: 10,
+    textAlign: 'center',
+    width: 200
+  },
+  listItem: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 300,
+    backgroundColor: 'grey'
+  }
+
+
+})
 
 export default App;
